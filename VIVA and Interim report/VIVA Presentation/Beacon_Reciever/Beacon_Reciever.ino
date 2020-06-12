cf14276359d17a5fb1c9bc148b0c86d32a0f2fc0
TravMcKee <45445832+TravMcKee@users.noreply.github.com>
2020-06-12 20:07:04 +1000

    Add files via upload

---
+++ VIVA and Interim report/VIVA Presentation/Beacon_Reciever/Beacon_Reciever.ino
@@ -0,0 +1,88 @@
+// Trav's beacon reciver ground segement
+ 
+#include <SPI.h>
+#include <RH_RF95.h>
+ 
+// for Feather32u4 RFM9x
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+ 
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+ 
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+ 
+// Blinky on receipt
+#define LED 13
+ 
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+ 
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+  delay(100);
+ 
+  Serial.println("Feather LoRa RX Test!");
+ 
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+    delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+ 
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+ 
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+ 
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+ 
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+ 
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(LED, HIGH);
+      RH_RF95::printBuffer("Received: ", buf, len);
+      Serial.print("Got: ");
+      
+      //int RxMSG[20] = (int *)buf;
+      for(int i = 0; i < sizeof(buf); i++)
+      {
+        Serial.print((int)buf); Serial.print(", ");
+      }
+      Serial.println();
+      Serial.print("number of Bytes is: ");
+      Serial.println(len);
+
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+}
