c8766bbb70229d86f100d90b24842db5555fc7cf
TravMcKee <45445832+TravMcKee@users.noreply.github.com>
2020-06-12 20:46:36 +1000

    Add files via upload

---
+++ Testing/Ground receiving station testing/Receive timing checks/Address packets/graph_address_data.m
@@ -0,0 +1,109 @@
+load data1.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data2.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data3.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data4.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data5.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data5.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data6.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data7.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data8.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data9.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
+
+load data10.mat
+figure
+plot(time,ch1*-1)
+hold on
+plot(time,(ch2/10))
+legend('Received LoRa signal','RXDone DIO0 interupt signal (scaled by 0.1)');
+title('Address Packet Lora signal with RXDone DIO0 interrupt signal using modem setting 0 (default)','fontSize',20);
+xlabel('Time (seconds)','fontSize',16);
+ylabel('Amplitude (volts)','fontSize',16);
